a6e299ac9497d505abfa94ef1182ededa4407d4a
Patrick Bogen <pdbogen-github@cernu.us>
2013-05-24 19:57:37 +0000

    Add hotkeys to main voting interface.

--- view_index.php
+++ view_index.php
@@ -17,12 +17,23 @@
 		$("#urlinput").val( $(img).attr( "src" ) );
 		$("#voteform").submit();
 	}
+	$(document).ready(function(){
+		$(document).keypress(function(ev){
+			if( ev.keyCode == 37 ) {
+				$("#leftimg").click();
+			} else if( ev.keyCode == 39 ) {
+				$("#rightimg").click();
+			} else if( ev.charCode == 114 ) {
+				location.reload();
+			}
+		});
+	});
 </script>
 
 <div style='margin-left: auto; margin-right: auto; text-align: center;'>
-	Click on the webcam that's better.<br/>
-	<img src='<?php print htmlentities( $img1[0] ); ?>' width='45%' onclick='vote(this);'>
-	<img src='<?php print htmlentities( $img2[0] ); ?>' width='45%' onclick='vote(this);'> <br/>
+	Click on the webcam (or press left or right arrow) that's better.<br/>
+	<img id='leftimg' src='<?php print htmlentities( $img1[0] ); ?>' width='45%' onclick='vote(this);'>
+	<img id='rightimg' src='<?php print htmlentities( $img2[0] ); ?>' width='45%' onclick='vote(this);'> <br/>
 	<div style='width:45%; float: left;'><?php print htmlentities( $img1[1] ); ?> vote(s)</div>
 	<div style='width:45%; float: right;'><?php print htmlentities( $img2[1] ); ?> vote(s)</div>
 </div>
